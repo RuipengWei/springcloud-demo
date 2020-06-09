1b2b26cabbf45952e88bd70c997bd9f8eaeb79e2
Ruipeng <1276978615@qq.com>
2020-06-09 17:32:04 +0800

    add zuul

--- springcloud-zuul-8090/src/main/java/com/ruipeng/springcloud/ZuulApplication.java
+++ springcloud-zuul-8090/src/main/java/com/ruipeng/springcloud/ZuulApplication.java
@@ -1,10 +1,19 @@
 package com.ruipeng.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 /**
  * @program: myDemo.com.ruipeng.springcloud.ZuulApplication
  * @description:
  * @author: Ruipeng
  * @create: 2020/6/9 17:13
  */
+@SpringBootApplication
+@EnableZuulProxy
 public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
 }
